5ac7fe8c266ec03be1fd328286df7cdd1564dff8
Olga Kotova <oliktva@gmail.com>
2018-04-19 20:07:46 +0300

    fix timer markup

--- js/components/timer-block.js
+++ js/components/timer-block.js
@@ -33,13 +33,12 @@ export default class TimerBlock extends AbstractView {
             cx="390" cy="390" r="370"
             class="timer-line"
             style="stroke-dashoffset: ${timerVisualEffect.dashoffset};stroke-dasharray: ${timerVisualEffect.dashaaray};filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center;"></circle>
-
-          <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-            <span class="timer-value-mins">${minutes}</span><!--
-            --><span class="timer-value-dots">:</span><!--
-            --><span class="timer-value-secs">${Timer.addZeroIfNeed(seconds)}</span>
-          </div>
         </svg>
+        <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+          <span class="timer-value-mins">${minutes}</span><!--
+          --><span class="timer-value-dots">:</span><!--
+          --><span class="timer-value-secs">${Timer.addZeroIfNeed(seconds)}</span>
+        </div>
       </div>`
     );
   }
